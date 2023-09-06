9137eb086d6bf338b7bd4cbfa525679669af08a6
munchys <fmoriniere@hotmail.fr>
2023-09-06 08:46:01 +0200

    ajout mission matos

--- src/components/accroche.vue
+++ src/components/accroche.vue
@@ -2,7 +2,8 @@
   <h2 class="titre-before souligne">Objectifs</h2>
   <div class="container-lg">
     <p>
-      Développeur logiciel avec 5 ans d'expériences passionné par le web Javascript et typescript
+      Développeur logiciel avec 5 ans d'expériences passionné par le web,
+      Javascript et Typescript
     </p>
   </div>
 </template>
